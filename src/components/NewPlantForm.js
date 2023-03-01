808156aeb216ecaee6dda40bb31cfa91cfb33a78
Eric Thvedt <ericsthvedt@gmail.com>
2023-03-01 09:37:05 -0800

    deliverables 1, 2, 3 all working

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onSubmit }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: 0
+  });
+
+  function handleFormChange(e) {
+    const key = e.target.name;
+    const value = e.target.value;
+    setFormData({...formData, [key]: value});
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(formData);
+    setFormData({
+      name: "",
+      image: "",
+      price: 0
+    })
+  };
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
-        <button type="submit">Add Plant</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleFormChange}/>
+        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleFormChange}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleFormChange}/>
+        <button type="submit" >Add Plant</button>
       </form>
     </div>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const jsonUrl = "http://localhost:6001/plants/";
 
 function PlantPage() {
+  
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch(jsonUrl)
+      .then(res => res.json())
+      .then(plants => setPlants(plants))
+  }, []);
+
+  function onSubmit(formData) {
+    fetch(jsonUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(plant => {
+        setPlants([...plants, plant])
+      });
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onSubmit={onSubmit}/>
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
